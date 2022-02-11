72cd8029a62364ede0006ddae4b60cef3567ca5a
sakshi panwar <75837550+sakshipanwar27@users.noreply.github.com>
2022-02-11 10:37:06 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,38 @@
+public class Str {
+
+	public static void main(String[] args) {
+//	 char[]  arr= {'s','a','r', 'a'};
+//		//String s1 = String.valueOf(arr);
+//		String s  = new String(arr);
+//		System.out.println(arr);
+//		
+//		// TODO Auto-generated method stub
+
+		String s = " SakshiPanwar  ";
+		String s1= "SaraPAnwar";
+//		s=s1.concat("saks");
+//		System.out.println(s);
+//		
+		System.out.println(s.charAt(4));
+		System.out.println(s.length());
+		System.out.println(s.substring(6));
+		System.out.println(s.substring(0, 6));
+		System.out.println(s.contains("Sa"));
+		System.out.println(s.equals(s1));
+		System.out.println(s==s1);
+		System.out.println(s.isEmpty());
+		System.out.println(s.replace('S','A'));
+		System.out.println(s.indexOf('P'));
+		System.out.println(s.toLowerCase());
+		System.out.println(s.toUpperCase());
+		System.out.println(s.valueOf('P'));
+		System.out.println(s.trim());
+		System.out.println(s.startsWith("Sa"));
+		
+		
+		
+		
+		
+	}
+
+}
